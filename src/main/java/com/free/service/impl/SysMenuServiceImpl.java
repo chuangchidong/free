16c00f184c1448395a2356230afa23b3684902ba
chuangchidong <1206214477@qq.com>
2017-06-28 10:03:25 +0800

    .

--- src/main/java/com/free/service/impl/SysMenuServiceImpl.java
+++ src/main/java/com/free/service/impl/SysMenuServiceImpl.java
@@ -72,7 +72,7 @@ public class SysMenuServiceImpl implements SysMenuService {
 		}
 
 		//用户权限列表
-		Set<String> permsSet = new HashSet<String>();
+		Set<String> permsSet = new HashSet<>();
 		for(String perms : permsList){
 			if(StringUtils.isBlank(perms)){
 				continue;
